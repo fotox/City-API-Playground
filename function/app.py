cd0c8acf02581c947b0f41841653b378bb62f1ef
fotox <carsten_stephan@mail.de>
2023-11-24 14:38:54 +0100

    - rename handler function

--- function/app.py
+++ function/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request
 
 from city_be.city import *
-from common.handler import check_get_response
+from common.handler import check_response
 
 API_PORT = 1337
 app = Flask(__name__)
@@ -11,20 +11,21 @@ app = Flask(__name__)
 def get_cities():
     dataset: list = get_city_from_database()
     message: str = "Cities found successfully"
-    return check_get_response(dataset, message)
+    return check_response(dataset, message)
 
 
 @app.route('/api/city/<city_id>', methods=['GET'])
 def get_city(city_id: str):
     dataset: list = get_city_from_database(city_id)
     message: str = "City found successfully"
-    return check_get_response(dataset, message)
+    return check_response(dataset, message)
 
 
 @app.route('/api/city', methods=['POST'])
 def create_city():
-    dataset = request.get_json()
-    return insert_city_into_database(dataset)
+    request_dataset: dict = request.get_json()
+    dataset, message = insert_city_into_database(request_dataset)
+    return check_response(dataset, message)
 
 
 @app.route('/api/city/<city_id>', methods=['PUT'])
