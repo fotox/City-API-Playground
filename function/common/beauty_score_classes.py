d310c3c8c379cc327a1cd6e3c4ea7f7cbd8a4432
fotox <carsten_stephan@mail.de>
2023-11-24 11:27:34 +0100

    - add sql queries for beauty score

--- function/common/beauty_score_classes.py
+++ function/common/beauty_score_classes.py
@@ -2,4 +2,8 @@ from enum import Enum
 
 
 class SelectBeautyData(Enum):
-    BEAUTY_SCORE = "SELECT * FROM beauty_score;"
+    BEAUTY_NAME = "SELECT * FROM beauty_score;"
+
+    BEAUTY_SCORE_BY_NAME = "SELECT id FROM beauty_score WHERE description LIKE %s;"
+
+    BEAUTY_NAME_BY_SCORE = "SELECT description FROM beauty_score WHERE id = %s;"
