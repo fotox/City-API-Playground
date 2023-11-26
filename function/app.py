10423556ee4ed24e339a93dd302cada6fc2dc9f9
fotox <carsten_stephan@mail.de>
2023-11-26 20:19:32 +0100

    - add error handling - add missing docstrings and typehints

--- function/app.py
+++ function/app.py
@@ -1,5 +1,5 @@
 from flasgger import Swagger
-from flask import Flask
+from flask import Flask, Response, request
 from flask_basicauth import BasicAuth
 
 from city_be.city import *
@@ -8,46 +8,46 @@ from sql_module.execution import execute_sql_by_script
 API_PORT: int = 1337
 INIT_DB_SCRIPT: str = 'sql_module/resources/0_0_1_init_scheme.sql'
 
-app = Flask(__name__)
+app: Flask = Flask(__name__)
 
 # Swagger documentation
-swagger = Swagger(app, template_file='swagger.yaml')
+swagger: Swagger = Swagger(app, template_file='swagger.yaml')
 
 # Init database basic authentication
 load_dotenv()
 app.config['BASIC_AUTH_USERNAME'] = os.getenv('BASIC_AUTH_USERNAME')
 app.config['BASIC_AUTH_PASSWORD'] = os.getenv('BASIC_AUTH_PASSWORD')
-basic_auth = BasicAuth(app)
+basic_auth: BasicAuth = BasicAuth(app)
 
 
 @app.route('/api/init_db', methods=['POST'])
 @basic_auth.required
-def init_database_scheme():
+def init_database_scheme() -> Response:
     return execute_sql_by_script(INIT_DB_SCRIPT)
 
 
 @app.route('/api/city', methods=['GET'])
-def get_cities():
+def get_cities() -> Response:
     return get_city_from_database()
 
 
 @app.route('/api/city/<city_id>', methods=['GET'])
-def get_city(city_id: str):
+def get_city(city_id: str) -> Response:
     return get_city_from_database(city_id)
 
 
 @app.route('/api/city', methods=['POST'])
-def create_city():
+def create_city() -> Response:
     return insert_city_into_database(request.get_json())
 
 
 @app.route('/api/city/<city_id>', methods=['PUT'])
-def update_city(city_id: str):
+def update_city(city_id: str) -> Response:
     return update_city_into_database(city_id, request.get_json())
 
 
 @app.route('/api/city/<city_id>', methods=['DELETE'])
-def delete_city(city_id: str):
+def delete_city(city_id: str) -> Response:
     return delete_city_from_database(city_id)
 
 
--- function/sql_module/execution.py
+++ function/sql_module/execution.py
@@ -1,5 +1,6 @@
 from flask import jsonify, Response
 
+from common.error_handlers import forbidden, not_found
 from log_module.log_app import viki_log
 from sql_module.connection import connect_to_postgres, disconnect_to_postgres
 
@@ -11,13 +12,11 @@ def execute_sql(connection: dict, execution: str) -> None:
     Execute the sql command on database
     :param connection: connection and cursor as dict
     :param execution: execution sql command
-    :return:
+    :return: None
     """
     try:
         connection["cursor"].execute(execution)
         connection["conn"].commit()
-
-        logger.info(f"Execution successfully")
 
     except Exception as e:
         logger.error(f"Cannot execute command, with error message: {e}")
@@ -29,19 +28,21 @@ def execute_sql_by_script(sql_file_path) -> Response:
     :param sql_file_path: path to executable sql file
     :return: connection and cursor object
     """
-    connection = connect_to_postgres()
+    connection: dict = connect_to_postgres()
 
     try:
         with open(sql_file_path, 'r') as sql_file:
             sql_script: str = sql_file.read()
+
     except Exception as e:
-        logger.error(f"Cannot read file: {sql_file_path}, with error message: {e}")
+        not_found(f"Cannot read file: {sql_file_path}, with error message: {e}")
 
     try:
         execute_sql(connection, sql_script)
+
     except Exception as e:
-        logger.error(f"Cannot execute command from file: {sql_file_path}, with error message: {e}")
+        forbidden(f"Cannot execute command from file: {sql_file_path}, with error message: {e}")
 
     disconnect_to_postgres(connection)
 
-    return jsonify({'message': 'Database initialization successfully'})
+    return jsonify({'message': 'Database initialization successfully'}, 200)
