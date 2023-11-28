790c2283d2e955c7eb7ac402c84303adb9c5be74
fotox <carsten_stephan@mail.de>
2023-11-28 17:49:46 +0100

    - change status code

--- function/common/error.py
+++ function/common/error.py
@@ -6,49 +6,49 @@ logger = viki_log("city_api")
 
 def bad_request(error):
     logger.debug(f"Error by bad_request: {error}")
-    return jsonify({'error': 'Bad Request', 'message': str(error)}), 400
+    return jsonify({'error': 'Bad Request', 'message': str(error), 'status': '400'})
 
 
 def unauthorized(error):
     logger.debug(f"Error by unauthorized: {error}")
-    return jsonify({'error': 'Unauthorized', 'message': str(error)}), 401
+    return jsonify({'error': 'Unauthorized', 'message': str(error), 'status': '401'})
 
 
 def forbidden(error):
     logger.debug(f"Error by forbidden: {error}")
-    return jsonify({'error': 'Forbidden', 'message': str(error)}), 403
+    return jsonify({'error': 'Forbidden', 'message': str(error), 'status': '403'})
 
 
 def not_found(error):
     logger.debug(f"Error by not_found: {error}")
-    return jsonify({'error': 'Not Found', 'message': str(error)}), 404
+    return jsonify({'error': 'Not Found', 'message': str(error), 'status': '404'})
 
 
 def method_not_allowed(error):
     logger.debug(f"Error by method_not_allowed: {error}")
-    return jsonify({'error': 'Method Not Allowed', 'message': str(error)}), 405
+    return jsonify({'error': 'Method Not Allowed', 'message': str(error), 'status': '405'})
 
 
 def conflict(error):
     logger.debug(f"Error by conflict: {error}")
-    return jsonify({'error': 'Conflict', 'message': str(error)}), 409
+    return jsonify({'error': 'Conflict', 'message': str(error), 'status': '409'})
 
 
 def unprocessable_entity(error):
     logger.debug(f"Error by unprocessable_entity: {error}")
-    return jsonify({'error': 'Unprocessable Entity', 'message': str(error)}), 422
+    return jsonify({'error': 'Unprocessable Entity', 'message': str(error), 'status': '422'})
 
 
 def too_many_requests(error):
     logger.debug(f"Error by too_many_requests: {error}")
-    return jsonify({'error': 'Too Many Requests', 'message': str(error)}), 429
+    return jsonify({'error': 'Too Many Requests', 'message': str(error), 'status': '429'})
 
 
 def internal_server_error(error):
     logger.debug(f"Error by internal_server_error: {error}")
-    return jsonify({'error': 'Internal Server Error', 'message': str(error)}), 500
+    return jsonify({'error': 'Internal Server Error', 'message': str(error), 'status': '500'})
 
 
 def service_unavailable(error):
     logger.debug(f"Error by service_unavailable: {error}")
-    return jsonify({'error': 'Service Unavailable', 'message': str(error)}), 503
+    return jsonify({'error': 'Service Unavailable', 'message': str(error), 'status': '503'})
