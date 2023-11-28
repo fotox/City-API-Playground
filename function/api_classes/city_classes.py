5d9103689420d71f4713f454c4f7c43182381056
fotox <carsten_stephan@mail.de>
2023-11-28 21:27:23 +0100

    - add valid_test_city data - fixed select statement in city_classes.py

--- function/api_classes/city_classes.py
+++ function/api_classes/city_classes.py
@@ -19,11 +19,11 @@ class SelectCityData(Enum):
             f"INNER JOIN {SCHEME}.beauty_score ON {SCHEME}.city.beauty = {SCHEME}.beauty_score.id "
             f"WHERE {SCHEME}.city.city_uuid = %s;")
 
-    POPULATION = f"SELECT {SCHEME}.population FROM {SCHEME}.city WHERE {SCHEME}.city_uuid = %s;"
+    POPULATION = f"SELECT {SCHEME}.city.population FROM {SCHEME}.city WHERE {SCHEME}.city.city_uuid = %s;"
 
-    LOCATION = (f"SELECT {SCHEME}.geo_location_latitude, {SCHEME}.geo_location_longitude "
+    LOCATION = (f"SELECT {SCHEME}.city.geo_location_latitude, {SCHEME}.city.geo_location_longitude "
                 f"FROM {SCHEME}.city "
-                f"WHERE {SCHEME}.city_uuid = %s;")
+                f"WHERE {SCHEME}.city.city_uuid = %s;")
 
 
 class DeleteCityData(Enum):
@@ -32,13 +32,14 @@ class DeleteCityData(Enum):
 
 
 class InsertCityData(Enum):
-    CITY = (f"INSERT INTO {SCHEME}.city ({SCHEME}.city_uuid, {SCHEME}.name, {SCHEME}.geo_location_latitude, "
-            f"{SCHEME}.geo_location_longitude, {SCHEME}.beauty, {SCHEME}.population) "
+    CITY = (f"INSERT INTO {SCHEME}.city ({SCHEME}.city.city_uuid, {SCHEME}.city.name, "
+            f"{SCHEME}.city.geo_location_latitude, {SCHEME}.city.geo_location_longitude, {SCHEME}.city.beauty, "
+            f"{SCHEME}.city.population) "
             f"VALUES (%s, %s, %s, %s, %s, %s)")
 
 
 class UpdateCityData(Enum):
     CITY = (f"UPDATE {SCHEME}.city "
-            f"SET {SCHEME}.name = %s, {SCHEME}.geo_location_latitude = %s, {SCHEME}.geo_location_longitude = %s, "
-            f"{SCHEME}.beauty = %s, {SCHEME}.population = %s "
-            f"WHERE {SCHEME}.city_uuid = %s")
+            f"SET {SCHEME}.city.name = %s, {SCHEME}.city.geo_location_latitude = %s, "
+            f"{SCHEME}.city.geo_location_longitude = %s, {SCHEME}.city.beauty = %s, {SCHEME}.city.population = %s "
+            f"WHERE {SCHEME}.city.city_uuid = %s")
