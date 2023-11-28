4573a6bbc69f72ca7458509ee4fce12bb050ded0
fotox <carsten_stephan@mail.de>
2023-11-28 17:50:16 +0100

    add default test cases with todos

---
+++ tests/test_common/test_city.py
@@ -0,0 +1,28 @@
+def test_create_app():
+    # TODO:
+    pass
+
+
+def test_get_beauty_score():
+    # TODO:
+    pass
+
+
+def test_get_city_from_database():
+    # TODO:
+    pass
+
+
+def test_delete_city_from_database():
+    # TODO:
+    pass
+
+
+def test_insert_city_into_database():
+    # TODO:
+    pass
+
+
+def test_update_city_into_database():
+    # TODO:
+    pass
---
+++ tests/test_common/test_events.py
@@ -0,0 +1,13 @@
+def test_load_alliances():
+    # TODO:
+    pass
+
+
+def insert_alliances():
+    # TODO:
+    pass
+
+
+def test_delete_alliances():
+    # TODO:
+    pass
